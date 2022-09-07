353ec93e6ba86916ec4c3fc504a651cc1f952e7e
Manuel Alcázar <manuzz3112@gmail.com>
2022-09-07 18:11:32 +0200

    pruebas axios

--- src/actions/productoActions.js
+++ src/actions/productoActions.js
@@ -1,14 +1,38 @@
-// import {
-//     AGREGAR_PRODUCTO,
-//     AGREGAR_PRODUCTO_EXITO,
-//     AGREGAR_PRODUCTO_ERROR
+import {
+    AGREGAR_PRODUCTO,
+    AGREGAR_PRODUCTO_EXITO,
+    AGREGAR_PRODUCTO_ERROR
 
-// } from '../types';
+} from '../types';
 
 //Crear nuevos productos
 
 export function crearNuevoProductoAction(producto) {
-    return () => {
-        console.log(producto)
+    return (dispatch) => {
+        dispatch(agregarProducto());
+
+        try {
+            dispatch(agregarProductoExito(producto))
+        } catch (error) {
+            dispatch(agregarProductoError(true))
+        }
     }
 }
+
+const agregarProducto = () => ({
+    type: AGREGAR_PRODUCTO,
+    payload: true
+});
+
+//si el producto se guarda en base de datos
+
+const agregarProductoExito = producto => ({
+    type: AGREGAR_PRODUCTO_EXITO,
+    payload: producto
+})
+
+//si hubo error
+const agregarProductoError = estado=> ({
+    type: AGREGAR_PRODUCTO_ERROR,
+    payload: estado
+})
--- src/reducers/productosReducer.js
+++ src/reducers/productosReducer.js
@@ -1,9 +1,9 @@
-// import {
-//     AGREGAR_PRODUCTO,
-//     AGREGAR_PRODUCTO_EXITO,
-//     AGREGAR_PRODUCTO_ERROR
+import {
+    AGREGAR_PRODUCTO,
+    AGREGAR_PRODUCTO_EXITO,
+    AGREGAR_PRODUCTO_ERROR
 
-// } from '../types';
+} from '../types';
 
 
 //cada reducer tiene su propio state
@@ -16,6 +16,23 @@ const initialState = {
 
 export default function state(state = initialState, action) {
     switch (action.type) {
+        case AGREGAR_PRODUCTO:
+            return {
+                ...state,
+                loading: action.payload
+            }
+        case AGREGAR_PRODUCTO_EXITO:
+            return {
+                ...state,
+                loading: false,
+                productos: [...state.productos, action.payload]
+            }
+        case AGREGAR_PRODUCTO_ERROR:
+            return {
+                ...state,
+                loading: false,
+                error: action.payload
+            }
         default:
             return state;
     }
